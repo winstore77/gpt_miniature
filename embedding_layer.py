db1c7028c601a49c8d51c1cf8dd8b99eb047c15a
winstore77 <152374136+winstore77@users.noreply.github.com>
2024-01-08 14:34:05 +0900

    Create embedding_layer.py

---
+++ embedding_layer.py
@@ -0,0 +1,12 @@
+class TokenAndPositionEmbedding(layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super().__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
+
+    def call(self, x):
+        maxlen = ops.shape(x)[-1]
+        positions = ops.arange(0, maxlen, 1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
